41f0f507982f6022ed6a226de32d10d39e5d6ed9
Piyumi Ranaweera <piyumiranaweera515@gmail.com>
2026-07-08 11:14:27 +0530

    feat: add Spring Boot 3 skeleton with PostgreSQL connection

---
+++ finsight-backend/src/test/java/com/piyumi/finsight_backend/FinsightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyumi.finsight_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinsightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
